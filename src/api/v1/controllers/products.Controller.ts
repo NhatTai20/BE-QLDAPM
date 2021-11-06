9561088fef09741145f320259e0e69dd89cfc165
Nguyễn Đình Hùng <nguyendinhhung152@gmail.com>
2021-11-06 16:41:35 +0700

    modify limit request

--- src/api/v1/controllers/products.Controller.ts
+++ src/api/v1/controllers/products.Controller.ts
@@ -11,19 +11,7 @@ import { search } from '../routes/routersApi/category.Router';
 // services
 import { productService } from '../services/product.Service';
 
-// validations
-// Website you wish to allow to connect
-		// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
 
-		// // Request methods you wish to allow
-		// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-		// // Request headers you wish to allow
-		// res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-		// // Set to true if you need the website to include cookies in the requests sent
-		// // to the API (e.g. in case you use sessions)
-		// res.setHeader('Access-Control-Allow-Credentials', 'true');
 
 class ProductsController {
 	//-----------------------------------GET----------------------------------
